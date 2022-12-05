1efb166c97d4b58217818169b516b5ec07bb36c6
Jesus <jesuspl@tutanota.com>
2022-12-05 07:14:32 +0100

    Initial commit

---
+++ app/src/main/java/com/jesuspinar/mailsample/fragments/Unread.java
@@ -0,0 +1,20 @@
+package com.jesuspinar.mailsample.fragments;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jesuspinar.mailsample.R;
+
+public class Unread extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_unread, container, false);
+    }
+}
